c697ed1277c990ae539461c12a9ee04830c0124b
Michael Montalbano <montabano1@gmail.com>
2020-03-19 12:54:58 -0400

    Initial Commit

---
+++ chatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by michael montalbano on 3/19/20.
+//  Copyright © 2020 michael montalbano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
